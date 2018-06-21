4cc91a549bb5f957e3d5a4d044b0e5328271b3a0
Liango Liu <l.liu@gbh.com.do>
2018-06-21 11:10:04 -0400

    Lecture 97 ReactJS

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,16 +1,47 @@
-import React, { Component } from 'react';
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class Global extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('search', this.state.query);
+    }
+
     render() {
         return (
-            <div>
+            <div className="Global">
                 <h2>Book Explorer!</h2>
                 <FormGroup>
                     <InputGroup>
-                        <FormControl type="text" placeholder="Search for a book" />
-                        <InputGroup.Addon>
-                            <Glyphicon glyph="search"></Glyphicon>
+                        <FormControl
+                            type="text"
+                            placeholder="Search for a book"
+                            onChange={event =>
+                                this.setState({query: event.target.value})
+                            }
+                            onKeyPress= {event =>
+                                {
+                                    if (event.key === 'Enter') {
+                                        this.search();
+                                    }
+                                }
+                            }
+                        />
+                        <InputGroup.Addon
+                            onClick={() =>
+                                this.search()
+                            }
+                        >
+                            <Glyphicon
+                                glyph="search"
+                            >
+                            </Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
